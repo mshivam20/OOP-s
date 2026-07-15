5c8ed8331d3aa7c400419337a4a176fcdf36a92e
Shivam Murkute <murkuteshivam1@gmail.com>
2026-07-15 15:16:26 +0530

    custom class

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+// custom class
+class Employee{
+    int id;
+    String name;
+    void printDetails(){
+        System.out.println("Employee Name is "+name);
+        System.out.println("Employee is is: "+id);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello World");
+
+        Employee shivam=new Employee();
+        shivam.id=5;
+        shivam.name="Shivam Murkute";
+
+        shivam.printDetails();
+
+
+
+    }
+}
